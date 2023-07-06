10371d1f17f9fccde682b6d736d23a409478acc5
Russ Martin <russ@quirkycube.com>
2023-07-06 14:45:51 -0400

    Temporarily force production logging

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b1b26f6edc14d0875cd95961e48186b8",
-"index.html": "3fb89bad6c7b5f4555d2000d2e5f9e85",
-"/": "3fb89bad6c7b5f4555d2000d2e5f9e85",
-"main.dart.js": "100501abe7eef5e1c9a4ca502ed16045",
+"index.html": "f95de4573bfde5d0be0eb7d09c3bb7ce",
+"/": "f95de4573bfde5d0be0eb7d09c3bb7ce",
+"main.dart.js": "b73c89f4c0e110241bf9d7bb0ba354fd",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
